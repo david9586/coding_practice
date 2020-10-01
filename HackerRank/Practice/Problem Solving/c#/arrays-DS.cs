8ee974894efb8a5335f13931252ad8ce480764b3
David King <david@doesfuzzylogictickle.com>
2020-10-01 19:06:38 -0400

    Arrays - ds

---
+++ HackerRank/Practice/Problem Solving/c#/arrays-DS.cs
@@ -0,0 +1,49 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the reverseArray function below.
+    static int[] reverseArray(int[] a) {
+        var j = a.Length - 1;
+        for(int i = 0; i < a.Length; i++){
+            if (j <= i){
+                break;
+            }
+            
+            var temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+
+            j--;
+        }
+
+        return a;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int[] res = reverseArray(arr);
+
+        textWriter.WriteLine(string.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
